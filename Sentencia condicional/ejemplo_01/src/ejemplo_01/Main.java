004478638d94c70fe569d0e0cdfe87f0c39dc2d0
Ricardo Fraga Méndez <ricaar.dev@gmail.com>
2023-06-01 14:13:07 +0200

    prueba-primer-ejercicio

---
+++ Sentencia condicional/ejemplo_01/src/ejemplo_01/Main.java
@@ -0,0 +1,23 @@
+/*
+
+*/
+
+package ejemplo_01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ricardo Fraga Méndez
+ */
+
+public class Main  {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+    }
+
+}
