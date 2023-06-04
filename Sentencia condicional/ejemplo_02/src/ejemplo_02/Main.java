7b44f0e62a12b6b9073ed0f54bed4598c5fd4c63
ricaardev <ricaar.dev@gmail.com>
2023-06-04 19:05:49 +0200

    código de ejemplo

--- Sentencia condicional/ejemplo_02/src/ejemplo_02/Main.java
+++ Sentencia condicional/ejemplo_02/src/ejemplo_02/Main.java
@@ -1,7 +1,6 @@
 /*
 
-*/
-
+ */
 package ejemplo_02;
 
 import java.util.Scanner;
@@ -10,14 +9,22 @@ import java.util.Scanner;
  *
  * @author Ricardo Fraga Méndez
  */
-
-public class Main  {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
+
+        System.out.print("¿Qué nota has sacado en el último examen? ");
+        String line = System.console().readLine();
+        double nota = Double.parseDouble(line);
+        if (nota >= 5) {
+            System.out.println("¡Enhorabuena!, ¡has aprobado!");
+        } else {
+            System.out.println("Lo siento, has suspendido.");
+        }
     }
 
 }
