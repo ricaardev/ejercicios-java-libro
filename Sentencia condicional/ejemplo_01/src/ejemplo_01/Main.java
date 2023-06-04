399fb3ef377fbde1358ea12232c56574df98e523
ricaardev <ricaar.dev@gmail.com>
2023-06-04 19:02:23 +0200

    código de ejemplo

--- Sentencia condicional/ejemplo_01/src/ejemplo_01/Main.java
+++ Sentencia condicional/ejemplo_01/src/ejemplo_01/Main.java
@@ -1,7 +1,6 @@
 /*
 
-*/
-
+ */
 package ejemplo_01;
 
 import java.util.Scanner;
@@ -10,14 +9,19 @@ import java.util.Scanner;
  *
  * @author Ricardo Fraga Méndez
  */
-
-public class Main  {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
+        System.out.print("¿Cuál es la capital de Kiribati? ");
+        String respuesta = System.console().readLine();
+        if (respuesta.equals("Tarawa")) {
+            System.out.println("¡La respuesta es correcta!");
+        } else {
+            System.out.println("Lo siento, la respuest");
+        }
     }
-
 }
