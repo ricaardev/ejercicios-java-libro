878acd7d20192649c13a9ec7b3024191d22497b6
ricaardev <ricaar.dev@gmail.com>
2023-06-05 10:32:25 +0200

    ejemplo_04

---
+++ Sentencia condicional/ejemplo_04/src/ejemplo_04/Main.java
@@ -0,0 +1,22 @@
+/*
+
+ */
+package ejemplo_04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ricardo Fraga Méndez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        System.out.println("prueba");
+    }
+
+}
